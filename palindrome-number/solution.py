a217b1e124740d32dc684e66040bbe816b639bd6
Daniel Dai <daeming@gmail.com>
2024-08-07 18:42:28 +0800

    wip

--- palindrome-number/solution.py
+++ palindrome-number/solution.py
@@ -13,3 +13,19 @@ class Solution:
         
         return xx == y;
         
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+        
+        reverse = 0
+        while x > reverse:
+            reverse = reverse * 10 + x % 10
+            x //= 10
+        
+        return x == reverse or x == reverse // 10 
