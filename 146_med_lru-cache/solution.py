57d34c0f6f3a6e8940073d7c413a1917e78439e4
Daniel Dai <daeming@gmail.com>
2024-08-11 08:14:14 +0800

    add

---
+++ 146_med_lru-cache/solution.py
@@ -0,0 +1,59 @@
+class Node (object):
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+        
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail
+        self.head.prev = self.tail
+        self.tail.next = self.head
+        self.tail.prev = self.head        
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.val        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self._remove(node)
+        else:
+            node = Node(key, value)
+            self.cache[key] = node
+        
+        self._add(node)
+        
+        if len(self.cache) > self.capacity:
+            del self.cache[self.head.next.key]
+            self._remove(self.head.next)        
+
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        node.prev = p
+        node.next = self.tail
+        self.tail.prev = node
+    
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
