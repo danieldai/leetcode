8857f16bd455e9c2e8517d903344c6d09e62e2e3
Daniel Dai <daeming@gmail.com>
2024-09-07 20:22:02 +0800

    wip

---
+++ 0160_easy_hot_intersection-of-two-linked-lists/problem.md
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+视频讲解
+
+https://www.bilibili.com/video/BV1eg411w7gn?p=15&vd_source=dead283aa3efb297fb9f9c1d2b8e9fe8
+
+ 
+热度
+字节、快手、腾讯、百度
+LeetCode热题HOT 100》专题
--- 0160_easy_hot_intersection-of-two-linked-lists/solution.py
+++ 0160_easy_hot_intersection-of-two-linked-lists/solution.py
@@ -30,8 +30,12 @@ class Solution2:
         p1 = headA
         p2 = headB
 
+        # when a pointer meet list end, go back to head of another head
         while p1 != p2:
             p1 = p1.next if p1 is not None else headB
             p2 = p2.next if p2 is not None else headA
         
         return p1
+    
+
+# 另一种方法，先计算出两个链表的长度，计算出长度差，在更长的链表上先走差值步数，然后两个指针一起同速移动
