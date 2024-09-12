c6069f5711b2af6473124da5619883604312f57d
Daniel Dai <daeming@gmail.com>
2024-09-12 18:26:58 +0800

    wip

---
+++ 0160_easy_hot_intersection-of-two-linked-lists/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ */
+
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+ }
+ 
+  
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        while(p1 != p2) {
+            p1 = p1 != null ? p1.next : headB;
+            p2 = p2 != null ? p2.next : headA; 
+        }
+
+        return p1;
+    }
+}
