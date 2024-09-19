b20b65f611098863ccc5d8895a6b76635f8acabd
Daniel Dai <daeming@gmail.com>
2024-09-19 17:54:36 +0800

    wip

---
+++ 0474_med_hot_ones-and-zeroes/problem.md
@@ -0,0 +1,6 @@
+https://leetcode.com/problems/ones-and-zeroes/description/
+
+
+https://programmercarl.com/0474.%E4%B8%80%E5%92%8C%E9%9B%B6.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE
+
+
---
+++ 0474_med_hot_ones-and-zeroes/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]  # 创建二维动态规划数组，初始化为0
+        for s in strs:  # 遍历物品
+            zeroNum = s.count('0')  # 统计0的个数
+            oneNum = len(s) - zeroNum  # 统计1的个数
+            for i in range(m, zeroNum - 1, -1):  # 遍历背包容量且从后向前遍历
+                for j in range(n, oneNum - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1)  # 状态转移方程
+        return dp[m][n]
