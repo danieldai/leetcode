5919eb53c411e69f3162527b85c6896481726fdb
Daniel Dai <daeming@gmail.com>
2024-09-20 11:12:16 +0800

    wip

---
+++ 0322_med_hot_coin-change/problem.md
@@ -0,0 +1,6 @@
+https://leetcode.com/problems/coin-change/description/
+
+
+https://programmercarl.com/0322.%E9%9B%B6%E9%92%B1%E5%85%91%E6%8D%A2.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE
+
+
---
+++ 0322_med_hot_coin-change/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+
+        dp[0] = 0
+
+        for i in range(len(coins)):
+            for j in range(coins[i], amount + 1):
+                if dp[j - coins[i]] != float('inf'):
+                    dp[j] = min(dp[j - coins[i]] + 1, dp[j])
+
+        if dp[amount] == float('inf'):
+            return -1
+        
+        return dp[amount]
+
+        
