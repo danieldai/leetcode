ca511fe4ce29f717089c6ff4a68edfc8b7fb694c
Daniel Dai <daeming@gmail.com>
2024-09-27 09:07:59 +0800

    wip

--- 0028_easy_hot_find-the-index-of-the-first-occurrence-in-a-string/problem.md
+++ 0028_easy_hot_find-the-index-of-the-first-occurrence-in-a-string/problem.md
@@ -7,4 +7,14 @@ https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
 https://programmercarl.com/0028.%E5%AE%9E%E7%8E%B0strStr.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE
 
 
-微软
+微软过去3个月
+微软Microsoft
+3
+Meta
+字节跳动
+苹果Apple
+Pocket Gems
+6个月前
+亚马逊
+5
+谷歌Google
--- 0028_easy_hot_find-the-index-of-the-first-occurrence-in-a-string/solution.py
+++ 0028_easy_hot_find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -1,8 +1,50 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        n = len(haystack)
+        m = len(needle)
+
+        if m == 0:
+            return 0
+        
+        nextp = [0] * m
+
+        j = 0
+        print(list(needle))
+        for i in range(1, m):
+            if j > 0 and needle[i] != needle[j]:
+                j = nextp[j - 1]
+
+            if needle[i] == needle[j]:
+                j += 1
+
+            nextp[i] = j
+            print([str(n) for n in nextp])
+        
+        j = 0
+        for i in range(n):
+            if j > 0 and haystack[i] != needle[j]:
+                j = nextp[j - 1]
+
+            if haystack[i] == needle[j]:
+                j += 1
+
+            if j == m:
+                return i - m + 1
+            
+        return -1
+
+
+
+
+
+class Solution2:
+    def strStr(self, haystack: str, needle: str) -> int:
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i:i+len(needle)] == needle:
                 return i
         
         return -1
         
+
+if __name__ == '__main__':
+    Solution().strStr("sadbutsad", "abcddabcx")
