c2ec9e85f442f55cd615b1c880afa90e7db8f819
Daniel Dai <daeming@gmail.com>
2024-10-17 15:12:20 +0800

    add

---
+++ 0452_med_hot_minimum-number-of-arrows-to-burst-balloons/solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int count = 1;
+        for (int i = 1; i < points.length; i++) {
+            if(points[i][0] > points[i - 1][1]) {
+                count++;
+            } else {
+                points[i][1] = Math.min(points[i][1], points[i -1][1]);
+            }
+        }
+
+        return count;
+    }
+}
