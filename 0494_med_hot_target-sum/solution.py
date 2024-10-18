fc54df9c524f7ff709c74399eb583d88c4ca049b
Daniel Dai <daeming@gmail.com>
2024-10-18 09:28:46 +0800

    wip

--- 0494_med_hot_target-sum/solution.py
+++ 0494_med_hot_target-sum/solution.py
@@ -13,12 +13,12 @@ class Solution:
         if (target + total) % 2 == 1:
             return 0
         
-        big_size = (target + total) // 2
+        bag_size = (target + total) // 2
 
-        dp = [[0] * (big_size + 1) for _ in range(len(nums))]
+        dp = [[0] * (bag_size + 1) for _ in range(len(nums))]
 
         # 初始化最上行
-        if nums[0] <= big_size:
+        if nums[0] <= bag_size:
             dp[0][nums[0]] = 1
 
         # 初始化最左列，最左列其他数值在递推公式中就完成了赋值
@@ -31,13 +31,13 @@ class Solution:
             dp[i][0] = 2 ** num_zero
 
         for i in range(1, len(nums)):
-            for j in range(0, big_size + 1):
+            for j in range(0, bag_size + 1):
                 if nums[i] > j:
                     dp[i][j] = dp[i - 1][j]
                 else:
                     dp[i][j] = dp[i - 1][j] + dp[i-1][j - nums[i]]
 
-        return dp[-1][big_size]
+        return dp[-1][bag_size]
 
 
         
